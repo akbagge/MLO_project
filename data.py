3709306fa194ae28b7fb6c6c5910064d41abd8f8
Hanus <hkh@pop-os.localdomain>
2023-01-13 12:34:35 +0100

    Docker created images and tries to run image, by python file is not correct

--- data.py
+++ data.py
@@ -13,7 +13,7 @@ file_path = "dataset.zip"
 #Download the dataset from Google Drive
 response = requests.get(url, stream=True)
 with open(file_path, "wb") as f:
-shutil.copyfileobj(response.raw, f)
+    shutil.copyfileobj(response.raw, f)
 
 #Extract the dataset
 shutil.unpack_archive(file_path, ".")
@@ -24,21 +24,21 @@ os.remove(file_path)
 #Create a PyTorch Lightning DataModule to handle the dataset
 
 class MyDataModule(pl.LightningDataModule):
-def init(self):
-super().__init__()
+    def init(self):
+        super().__init__()
 
-def setup(self, stage=None):
-    # Load and preprocess the dataset
-    self.dataset = MyDataset()
-    
-    # Split the dataset into training and test sets
-    self.train_dataset, self.test_dataset = random_split(self.dataset, [0.8, 0.2])
-    
-def train_dataloader(self):
-    return DataLoader(self.train_dataset, batch_size=32)
+    def setup(self, stage=None):
+        # Load and preprocess the dataset
+        self.dataset = MyDataset()
+        
+        # Split the dataset into training and test sets
+        self.train_dataset, self.test_dataset = random_split(self.dataset, [0.8, 0.2])
+        
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset, batch_size=32)
 
-def val_dataloader(self):
-    return DataLoader(self.test_dataset, batch_size=32)
+    def val_dataloader(self):
+        return DataLoader(self.test_dataset, batch_size=32)
 
 #Initialize the DataModule
 data_module = MyDataModule()
