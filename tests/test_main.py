c7a5004f5110dc96be1eece1bf807593627eb002
Hanus Krett Hansen <hanus.krett@gmail.com>
2023-01-19 14:28:29 +0100

    Pushed for sharing

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,25 +1,20 @@
 import pytest
-from src import create_app
+import src
+from timm.models.resnet import resnet50
+import os
+from src.models.predict_model import RiceModel
+import timm
+from pprint import pprint
+from flask import Flask, request, flash, render_template
+from werkzeug.utils import secure_filename
+
+from src.api import main
 
 @pytest.fixture()
 def app():
-    app = create_app()
-    app.config.update({
-        "TESTING": True,
-    })
-
-    # other setup can go here
-
-    yield app
-
-    # clean up / reset resources here
-
+    main.app.requst
 
 @pytest.fixture()
 def client(app):
     return app.test_client()
 
-
-@pytest.fixture()
-def runner(app):
-    return app.test_cli_runner()
